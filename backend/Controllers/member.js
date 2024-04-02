9b2242f541e25a087cb1b1a3c4dc91606cdb9db3
Don <daoduylongpy@gamil.com>
2024-04-02 12:45:35 +0700

    BackEnd

---
+++ backend/Controllers/member.js
@@ -0,0 +1,105 @@
+const memberService = require("../services/member");
+const AppError = require("../helpers/appError");
+
+module.exports = class member {
+  static async apiCreatemember(req, res, next) {
+    try {
+      if (!req.body) return next(new AppError("No form data found", 404));
+
+      const createdmember = await memberService.creatmember(req.body);
+
+      res.status(200).json({
+        member: createdmember,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiListImagemember(req, res, next) {
+    try {
+      if (!req.body) return next(new AppError("No form data found", 404));
+
+      const allmember = await memberService.listAllmember(req.body);
+
+      res.status(200).json(
+        allmember.map((item) => ({
+          _id: item._id,
+          image: item.image,
+        }))
+      );
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiHistorymember(req, res, next) {
+    try {
+      if (!req.body) return next(new AppError("No form data found", 404));
+
+      const allmember = await memberService.listAllmember(req.body);
+
+      res.status(200).json(
+        allmember.map((item) => ({
+          _id: item._id,
+          name: item.name,
+          image: item.image,
+          time: item.active[0] ? item.active[0] : null,
+        }))
+      );
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiStaticmember(req, res, next) {
+    try {
+      if (!req.body) return next(new AppError("No form data found", 404));
+
+      const allmember = await memberService.listAllmember(req.body);
+
+      res.status(200).json(
+        allmember.map((item) => ({
+          _id: item._id,
+          name: item.name,
+          image: item.image,
+          count: item.active.length
+        })).sort((a, b) => b.count - a.count)
+      );
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+
+  static async apiDeletemember(req, res, next) {
+    try {
+      const deleteResponse = await memberService.deletemember(req.params.id);
+      res.status(200).json(deleteResponse);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiExtendmember(req, res, next) {
+    try {
+      if (!req.body) return next(new AppError("No form data found", 404));
+      const updated = await memberService.updatemember(req.params.id,req.body.dateEnd);
+
+      if (updated.modifiedCount === 0) {
+        throw new Error("Unable to update , error occord");
+      }
+
+      res.status(200).json(updated);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+
+
+
+
+
+
+};
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,48 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3001;
-app.get("/",(req,res)=>{
-	res.send("Hello")
-})
-app.listen(PORT, console.log("Server don start for port: " + PORT))
+// requiring database
+require("./mongoDB/config");
+require('dotenv').config();
+
+// error
+const AppError = require("./helpers/appError");
+const errorHandler = require("./helpers/errorHandler");
+const headers = require("./helpers/headers");
+
+
+const express = require("express");
+const cors = require("cors"); 
+const bodyParser =  require("body-parser");
+
+const router = require("./router/index");
+const app = express();  
+const port = process.env.port;
+  
+  
+
+//config API routes;  
+app.use(cors())
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false }));  
+app.use(router) 
+ 
+// for testing index page 
+// app.get("/", (req, res) => {
+//       res.send(`<h1>Hello!</h1>`)
+//   });
+  
+
+  // node js apperror class (error) extanding  
+app.all("*", (req, res, next) => { 
+    next(new AppError(`The URL ${req.originalUrl} does not exists`, 404));
+  });
+  
+app.use(headers);
+// using errors handler
+app.use(errorHandler);
+
+
+app.listen(port, () => {
+    console.log(`Application is listening at port ${port}`); 
+});
+ 
+
+module.exports = app;
