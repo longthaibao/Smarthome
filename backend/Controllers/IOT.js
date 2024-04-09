6c627a2c711542839de519a8e64a8cd2e074c897
Don <daoduylongpy@gamil.com>
2024-04-09 09:44:26 +0700

    updatebackend

--- backend/Controllers/IOT.js
+++ backend/Controllers/IOT.js
@@ -1,5 +1,8 @@
 const IOTService = require("../services/IOT");
 const AppError = require("../helpers/appError");
+const { spawn } = require('child_process');
+
+
 
 module.exports = class IOT {
 
@@ -16,6 +19,31 @@ module.exports = class IOT {
     static async apiOpenIOT(req, res, next) { 
         try { 
           // CODE IOT
+          res.status(200).json(true);
+        } catch (error) {
+          res.status(500).json({ error: error });
+        }
+      }
+
+      static async apiFaceAuthorization(req, res, next) { 
+        try { 
+
+
+            // test Python
+            console.log("Test pythons");
+            let pythonProcess = spawn('python', ['config/test.py', 'arg134', 'arg2']);
+            pythonProcess.stdout.on('data', (data) => {
+                console.log(`Kết quả từ Python: ${data}`);
+            });
+            pythonProcess.stderr.on('data', (data) => {
+                console.error(`Lỗi từ Python: ${data}`);
+            });
+
+
+
+
+
+
           res.status(200).json(true);
         } catch (error) {
           res.status(500).json({ error: error });
--- backend/router/index.js
+++ backend/router/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const adminCtrl = require("../controllers/admin");
 const memberCtrl = require("../controllers/member");
 const IOTCtrl = require("../controllers/IOT");
+const uploadCloud = require("../config/cloudinary.config");
 
 // admin routes
 router.post("/admin/login", adminCtrl.apiCheckadmin);
@@ -10,7 +11,7 @@ router.post("/admin/register", adminCtrl.apiCreateadmin);
 router.put("/admin/edit/:id",adminCtrl.apiEditadmin);
 
 // member routes
-router.post("/member/register", memberCtrl.apiCreatemember);
+router.post("/member/register", uploadCloud.single("images"), memberCtrl.apiCreatemember);
 router.get("/member/list", memberCtrl.apiListImagemember);
 router.get("/member/history", memberCtrl.apiHistorymember);
 router.get("/member/static", memberCtrl.apiStaticmember);
@@ -20,6 +21,6 @@ router.put("/member/extend/:id", memberCtrl.apiExtendmember);
 // IOT router
 router.get("/IOT/lastAuthorization", IOTCtrl.apiLastAuthorizationIOT);
 router.get("/IOT/open", IOTCtrl.apiOpenIOT);
-
+router.get("/IOT/FaceAuthorization", uploadCloud.array("images"), IOTCtrl.apiFaceAuthorization);
 
 module.exports = router;
